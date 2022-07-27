19b4ea1a82ff2e8ada71ee667b909735fcaef007
BhavinOndhiya <62885659+BhavinOndhiya@users.noreply.github.com>
2022-07-27 10:24:26 +0530

    Update p7.js

--- p7.js
+++ p7.js
@@ -6,46 +6,47 @@ const mixed = ['str', 89, [3, 5]];
 
 const arr = new Array(23, 123, 21, 'orange');
 console.log(arr);
-// console.log(mixed);
-// console.log(fruits);
+console.log(mixed);
+console.log(fruits);
 
 //IS ARRAY or NOT AN ARRAY
 //with help of these array function we can if it is an array or not an array
-// console.log(Array.isArray(fruits));
-// console.log(Array.isArray('dfdfef'));
+ console.log(Array.isArray(fruits));
+console.log(Array.isArray('dfdfef'));
 arr[0] = 'harry';
 let arrelement[0] = arr[0];
-// console.log('element :', arrelement);
-// console.log(arr)
+console.log('element :', arrelement);
+console.log(arr)
 
 //Index Number of an array
 //with help of these you can find index of any value which is inside the array
 let value = marks.indexOf(95);
-// console.log(value);
+console.log(value);
 
 //MUTATING OR MODIFYING ARRAYS
 
 //Push newly defined array to last
-// marks.push(3564);
+marks.push(3564);
 
 //Push newly defined array to first
-//marks.unshift(4520);
+marks.unshift(4520);
 
 //these function would remove the last element of existing array
-//marks.pop();
+marks.pop();
 
 //these function would remove the first element of existing array
-//marks.shift()
+marks.shift()
 
 //Remove Selected elements from An Array
 //means if u kept 0,2 it means start from zero and remove 2 elements from it
-//marks.splice(0,2)
+marks.splice(0,2)
 
 //REVERSING AN ARRAY
 //it would completely reverse the element of the array
+marks.reverse();
 
 //CONCAT
-//marks.concat(new one which u want to concat);
+marks.concat(new one which u want to concat);
 
 let myobj = {
     name: 'harry',
