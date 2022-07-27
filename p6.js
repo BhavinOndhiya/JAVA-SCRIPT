cd88d837cd238735d568a789ff962b7ea20c4c08
BhavinOndhiya <62885659+BhavinOndhiya@users.noreply.github.com>
2022-07-27 10:04:28 +0530

    Update p6.js

--- p6.js
+++ p6.js
@@ -8,39 +8,39 @@ html = "<h1> this is heading</h1>" + "<p> this is my paragraph</p>";
 
 html = html.concat('this', 'str2');
 console.log(html);
-// console.log(length.html);
-// console.log(html.toLowerCase());
-// console.log(html.toUpperCase());
+console.log(length.html);
+console.log(html.toLowerCase());
+console.log(html.toUpperCase());
 
-// //console.log(html[1]);
-// console.log(html.indexOf('this'));
-// console.log(html.indexOf('<'));
-// console.log(html.lastIndexOf('<'));
-// console.log(html.charAt('3'));
+console.log(html[1]);
+console.log(html.indexOf('this'));
+console.log(html.indexOf('<'));
+console.log(html.lastIndexOf('<'));
+console.log(html.charAt('3'));
 // //if it ends with str2 it return true else false
-// console.log(html.endsWith('str2'));
+console.log(html.endsWith('str2'));
 // //if your code includes 'is' then output is true else false. 
-// console.log(html.includes('is'));
-// console.log(html.includes('gsaekjjfbakj'));
+console.log(html.includes('is'));
+console.log(html.includes('gsaekjjfbakj'));
 // //if you want first multiple words or last it can be given with the help of slice(x,y)
 
-// console.log(html.slice(0, 5));
-// console.log(html.slice(-4));
+console.log(html.slice(0, 5));
+console.log(html.slice(-4));
 
 //in substring you cant get minus element instead you will be given full string
-// console.log(html.substring(0, 5));
-// console.log(html.substring(-4));
+console.log(html.substring(0, 5));
+console.log(html.substring(-4));
 
 //SPLIT Function
 //with help of these we can split our string into small array
 
-// console.log(html.split('>'));
-// console.log(html.split('>'));
+console.log(html.split('>'));
+console.log(html.split('>'));
 
 //REPLACE FUNCTION
 //It replaces the first occurence only
 
-// console.log(html.replace('this', 'it'));
+console.log(html.replace('this', 'it'));
 
 
 //Writing HTML inside the js script through these method.
